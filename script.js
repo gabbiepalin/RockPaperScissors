edefb7f653289d4f70bdde6278f261e56c38152e
gabbiepalin <gabrielle.palin17@gmail.com>
2023-07-15 20:59:44 +1000

    added variables created the instruction to return what words if chosen

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 function getComputerChoice() {
-    
+    //generate a random number between zero and two
+    const randomNumber = Math.floor(Math.random() * 3);
+
+    //make computer pick a random number for choice (0 = "rock", 1 = "paper", 2 = "scissors")
+    switch (randomNumber) {
+        case 0:
+            return "rock";
+        case 1:
+            return "paper";
+        case 2:
+            return "scissors";
+    }
 }
+
